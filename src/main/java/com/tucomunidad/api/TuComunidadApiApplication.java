f21b0bbcabaa9e1c2176a90affdc153dbce69304
AlvaroG001 <alvarogarciaortiz7@gmail.com>
2024-04-17 11:53:27 +0200

    first commit

---
+++ src/main/java/com/tucomunidad/api/TuComunidadApiApplication.java
@@ -0,0 +1,13 @@
+package com.tucomunidad.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuComunidadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuComunidadApiApplication.class, args);
+	}
+
+}
